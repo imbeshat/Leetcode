cd1aa1343f8e6359dbbe6a4c5e1c0b0efe4a53c9
Imbeshat Aslam <48837703+imbeshat@users.noreply.github.com>
2023-01-13 16:11:04 +0530

    Time: 91 ms (72.73%), Space: 43.9 MB (78.54%) - LeetHub

---
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function(nums) {
+    let output=[];
+    for(let i=0;i<nums.length;i++){
+        let count=0;
+        for(let j=0;j<nums.length;j++){
+            if(nums[i]>nums[j]){
+            count++;
+            }  
+        }
+        output.push(count);
+    }
+    return output;
+};
