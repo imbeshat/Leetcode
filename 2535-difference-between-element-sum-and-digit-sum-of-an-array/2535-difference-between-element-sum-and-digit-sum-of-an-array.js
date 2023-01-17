3d5230b797bfad804aa770fbdc5e41618e7ad861
Imbeshat Aslam <48837703+imbeshat@users.noreply.github.com>
2023-01-17 13:59:03 +0530

    Time: 72 ms (69.23%), Space: 44.4 MB (58.37%) - LeetHub

--- 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.js
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.js
@@ -4,10 +4,6 @@
  */
 var differenceOfSum = function(nums) {
     const elementSum=nums.reduce((a,b)=>a+b,0);
-    let allDigitSum=0;
-    for (let i=0;i<nums.length;i++){
-        const digitSum=nums[i].toString().split('').map(Number).reduce((a,b) => a + b,0);
-        allDigitSum+=digitSum;
-    }
+    let allDigitSum=nums.join("").split("").reduce((a, b) => +a + +b, 0);
     return Math.abs(elementSum-allDigitSum);
 };
