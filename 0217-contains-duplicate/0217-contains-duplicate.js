5736f9c90bb6f18aff766c4fc0e27eddcd6b20f2
Imbeshat Aslam <48837703+imbeshat@users.noreply.github.com>
2023-05-29 14:11:39 +0530

    Time: 93 ms (62.84%), Space: 59.2 MB (7.78%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,11 +3,19 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    nums.sort();
-    for(let i=0; i<nums.length-1; i++){
-        if(nums[i] == nums[i+1]){
-            return true;
-        }
-    }
-    return false;
+    // nums.sort();
+    // for(let i=0; i<nums.length-1; i++){
+    //     if(nums[i] == nums[i+1]){
+    //         return true;
+    //     }
+    // }
+    // return false;
+    let map = new Map();
+	for (let i = 0; i < nums.length; i++) {
+		if (map.has(nums[i])) {
+			return true;
+		}
+		map.set(nums[i], i);
+	}
+	return false;
 };
