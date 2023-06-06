068c3d99c3ac49bc7a1f791bd975dca28d8fa56b
Imbeshat Aslam <48837703+imbeshat@users.noreply.github.com>
2023-06-06 14:04:13 +0530

    Time: 72 ms (52.25%), Space: 44.1 MB (23.41%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if (n===1)
+        return true
+    if (n%2===1 || n===0)
+        return false
+    return isPowerOfTwo(n/2)
+};
